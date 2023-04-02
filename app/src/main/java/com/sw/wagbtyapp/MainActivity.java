0a6675a51580ee0d11986e9478941725fb3bd79a
wejdanmurad <annoying0597>
2023-04-02 13:56:20 +0300

    sh data

--- app/src/main/java/com/sw/wagbtyapp/MainActivity.java
+++ app/src/main/java/com/sw/wagbtyapp/MainActivity.java
@@ -2,18 +2,38 @@ package com.sw.wagbtyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         changeClor();
+        initView();
+        setNavigationGraph();
+    }
+
+    private void initView() {
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+    }
+
+    private void setNavigationGraph() {
+        NavController navController = Navigation.findNavController(this, R.id.my_nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigation, navController);
+        bottomNavigation.setItemIconTintList(null);
     }
 
 
